bb118866856cc7b44854601f12cca8c4b177f5de
Utsav Singh <singhutsavofficial@gmail.com>
2024-05-15 02:35:18 +0530

    file validity and existence is checked before opening it

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,34 @@
 
 import dependancyGraph from "../classes/dependancyGraph";
 import path from "path"
-export function resolveRequest(filePath, requestedPath) {
-    let resolvedPath= path.join(path.dirname(filePath), requestedPath);
-    if (resolvedPath.slice(0,resolvedPath.length-3) !== ".js") resolvedPath+=".js"
+const fs = require('fs')
+function resolveRequest(filePath, requestedPath) {
+    let resolvedPath = path.join(path.dirname(filePath), requestedPath);
     return resolvedPath
 }
 
-export function  createDependancyGraph(input) {
-    return new dependancyGraph(input); 
+export function checkJsFileExistsAndResolveRequest(filePath, requestedPath) {
+    let absolutePath = resolveRequest(filePath, requestedPath)
+
+    try {
+        if (!absolutePath.endsWith('.js')) absolutePath += '.js'
+        // Check if the file exists
+        const stats = fs.statSync(absolutePath);
+
+        // Check if it's a file and has a ".js" extension
+        if (stats.isFile()) {
+            return { absolutePath, fileExists: true };
+        }
+    } catch (err) {
+        // Handle errors (e.g., file not found)
+        if(err.code === 'ENOENT') console.log(`${absolutePath} does not exist`)
+        return { absolutePath: null, fileExists: false };
+    }
+
+    return { absolutePath: null, fileExists: false };
+}
+
+
+export function createDependancyGraph(input) {
+    return new dependancyGraph(input);
 }
