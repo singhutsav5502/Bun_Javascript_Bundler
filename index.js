a45525e1283e4aa64b8c44070688730faba8af09
Utsav Singh <singhutsavofficial@gmail.com>
2024-05-14 21:40:21 +0530

    1. Setup CLI argument vector based endPoint input. 2. Validate end points using Bun.file() and partially supported node fs ( used for validating target directory existence )

---
+++ index.js
@@ -0,0 +1,49 @@
+import { parseArgs } from "util";
+/***********************************************/
+// node fs partilly supported by bun. 
+// used here for exit directory path validation
+/***********************************************/
+const fs = require('fs/promises');
+
+
+/***********************************************/
+// GET END POINTS
+/***********************************************/
+const { values: endPoints, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    entryPoint: {
+      type: 'string',
+    },
+    exitPoint: {
+      type: 'string',
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+});
+
+
+//  validate entry file
+async function validateEndPoints() {
+  const file = Bun.file(endPoints?.entryPoint);
+  const entryPointExists = await file.exists();
+  if (!entryPointExists) {
+    throw new Error("Entry file does not exist\nAborting task.")
+  }
+  const exitDirectoryPath = endPoints.exitPoint;
+  try {
+    await fs.access(exitDirectoryPath, fs.constants.F_OK);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log("Exit Directory does not exist.");
+    } else {
+      console.error("An error occurred:", err);
+    }
+  }
+};
+
+function main() {
+  validateEndPoints();
+}
+main();
